661f140ac858dd75efcbe804c0eec144bd27db32
Frida Gidlund <frida.gidlund@sogeti.com>
2020-12-02 00:02:56 +0100

    Day 1 complete

---
+++ AdventOfCode2020/InputReader.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020
+{
+    public static class InputReader
+    {
+        static readonly string folderPath = @"C:\Users\fgidlund\source\repos\AdventOfCode2020\AdventOfCode2020\Inputs\";
+        
+        public static string[] ReadFile(string fileName)
+        {
+            return System.IO.File.ReadAllLines(folderPath + fileName);
+        }
+
+        public static List<int> GetInts(string[] input)
+        {
+            var output = new List<int>();
+            foreach (var item in input)
+            {
+                output.Add(Int32.Parse(item.Trim()));
+            }
+            return output;
+        }
+    }
+}
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -11,7 +11,30 @@ namespace AdventOfCode2020
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine(Day1().ToString());
+
             Console.ReadKey();
+        }
+
+        static int Day1()
+        {
+            var input = InputReader.GetInts(InputReader.ReadFile("day1.txt"));
+            //Todo Don't do this
+            while (input.Count > 2)
+            {
+                for (int i = input.Count() - 1; i > 1; i--)
+                {
+                    for (int j = input.Count() - 2; j > 0; j--)
+                    {
+                        if (input[0] + input[i] + input[j] == 2020)
+                        {
+                            return input[0] * input[i] * input[j];
+                        }
+                    }
+                }
+                input.Remove(input[0]);
+            }
+            return 0;
         }
     }
 }
