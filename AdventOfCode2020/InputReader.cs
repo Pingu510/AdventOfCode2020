165a8df84c4cc1f15fd49e55ea8fe22de4d38826
Frida Gidlund <frida.gidlund@regionvasterbotten.se>
2020-12-03 11:45:34 +0100

    Day2 complete

--- AdventOfCode2020/InputReader.cs
+++ AdventOfCode2020/InputReader.cs
@@ -5,8 +5,9 @@ namespace AdventOfCode2020
 {
     public static class InputReader
     {
-        static readonly string folderPath = @"C:\Users\fgidlund\source\repos\AdventOfCode2020\AdventOfCode2020\Inputs\";
-        
+        //static readonly string folderPath = @"C:\Users\fgidlund\source\repos\AdventOfCode2020\AdventOfCode2020\Inputs\";
+        static readonly string folderPath = @"C:\Users\frgi02\source\repos\Pingu510\AdventOfCode2020\AdventOfCode2020\Inputs\";
+
         public static string[] ReadFile(string fileName)
         {
             return System.IO.File.ReadAllLines(folderPath + fileName);
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -11,7 +11,9 @@ namespace AdventOfCode2020
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.WriteLine(Day1().ToString());
+            //Console.WriteLine(Day1().ToString());
+            Console.WriteLine(Day2().ToString());
+
 
             Console.ReadKey();
         }
@@ -35,6 +37,35 @@ namespace AdventOfCode2020
                 input.Remove(input[0]);
             }
             return 0;
+        }
+
+        static int Day2()
+        {
+            int count = 0;
+            var input = InputReader.ReadFile("day2.txt");
+            foreach (var row in input)
+            {
+                //1-3 a: abcde
+                var items = row.Replace(":", String.Empty).Split(' ', '-');
+
+                //Part 1
+                //var occurance = items[3].Count(x => x == Char.Parse(items[2]));
+                //if (occurance >= Int32.Parse(items[0]) && occurance <= Int32.Parse(items[1]))
+                //{
+                //    count++;
+                //}
+
+                //Part 2
+                var match = Char.Parse(items[2]);
+                var pos1 = items[3][Int32.Parse(items[0]) - 1] == match;
+                var pos2 = items[3][Int32.Parse(items[1]) - 1] == match;
+
+                if (pos1 && !pos2 || !pos1 && pos2)
+                {
+                    count++;
+                }
+            }
+            return count;
         }
     }
 }
